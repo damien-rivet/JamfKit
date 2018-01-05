134369e553c27390ac4b4d5870edf9f96dca5d61
Damien R <damien.rivet@gmail.com>
2018-01-05 10:13:05 +0100

    Implement Computer command model

---
+++ JamfKit/Tests/Models/Computer/ComputerCommandTests.swift
@@ -0,0 +1,74 @@
+//
+//  ComputerCommandTests.swift
+//  JAMFKit
+//
+//  Copyright © 2018 JAMFKit. All rights reserved.
+//
+
+import XCTest
+
+@testable import JAMFKit
+
+class ComputerCommandTests: XCTestCase {
+
+    // MARK: - Constants
+
+    let subfolder = "Computer/"
+    let defaultCommand = "command"
+    let defaultPasscode: UInt = 123456
+
+    // MARK: - Tests
+
+    func testShouldInitializeFromJSON() {
+        let payload = self.payload(for: "computer_command_valid", subfolder: subfolder)!
+
+        let actualValue = ComputerCommand(json: payload)
+
+        XCTAssertNotNil(actualValue)
+        XCTAssertEqual(actualValue?.description, "[ComputerCommand][\(defaultCommand)]")
+        XCTAssertEqual(actualValue?.general.command, defaultCommand)
+        XCTAssertEqual(actualValue?.general.passcode, defaultPasscode)
+        XCTAssertEqual(actualValue?.computers.count, 1)
+    }
+
+    func testShouldInitializeFromIncompleteJSON() {
+        let payload = self.payload(for: "computer_command_incomplete", subfolder: subfolder)!
+
+        let actualValue = ComputerCommand(json: payload)
+
+        XCTAssertNotNil(actualValue)
+        XCTAssertEqual(actualValue?.description, "[ComputerCommand][\(defaultCommand)]")
+        XCTAssertEqual(actualValue?.general.command, defaultCommand)
+        XCTAssertEqual(actualValue?.general.passcode, defaultPasscode)
+        XCTAssertEqual(actualValue?.computers.count, 0)
+    }
+
+    func testShouldNotInitializeFromInvalidJSON() {
+        let payload = self.payload(for: "computer_command_invalid", subfolder: subfolder)!
+
+        let actualValue = ComputerCommand(json: payload)
+
+        XCTAssertNil(actualValue)
+    }
+
+    func testShouldEncodeToJSON() {
+        let payload = self.payload(for: "computer_command_valid", subfolder: subfolder)!
+
+        let actualValue = ComputerCommand(json: payload)
+        let encodedObject = actualValue?.toJSON()
+
+        XCTAssertNotNil(encodedObject)
+        XCTAssertEqual(encodedObject?.count, 2)
+        XCTAssertNotNil(encodedObject?[ComputerCommand.GeneralKey])
+        XCTAssertNotNil(encodedObject?[ComputerCommand.GeneralKey])
+        XCTAssertNotNil(encodedObject?[ComputerCommand.ComputersKey])
+
+        let generalNode = encodedObject?[ComputerCommand.GeneralKey] as! [String: Any]
+        XCTAssertNotNil(generalNode[ComputerCommandGeneral.CommandKey])
+        XCTAssertNotNil(generalNode[ComputerCommandGeneral.PasscodeKey])
+
+        let computersNode = encodedObject?[ComputerCommand.ComputersKey] as! [[String: [String: UInt]]]
+        XCTAssertEqual(computersNode.count, 1)
+        XCTAssertEqual(computersNode[0]["computer"]!["id"], 1)
+    }
+}
